5bda080943f81da7ef7c6f1c63f488a973076111
Televizor1945 <kurochkin.egor1993@gmail.com>
2022-11-27 19:47:32 +0300

    Add profile endpoint

--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,11 +1,36 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login, logout
 
-from rest_framework import generics
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-from .serializers import RegistrationSerializer
+from . import serializers
 
 USER_MODEL = get_user_model() # return model User (/core/models.py)
 
 class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
-    serializer_class = RegistrationSerializer
+    serializer_class = serializers.RegistrationSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = serializers.LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.ProfileSerializer
+    queryset = USER_MODEL.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
