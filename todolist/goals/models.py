b9a8e7915b31ce340d9e840e0a50ca8043c295a5
Televizor1945 <kurochkin.egor1993@gmail.com>
2022-12-06 23:11:54 +0300

    Add Board and BoardParticipant. Add mixin in utils.py. Mmake migrate

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -14,6 +14,7 @@ class Board(DatesModelMixin):
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
 
 
+
 class BoardParticipant(DatesModelMixin):
     class Meta:
         unique_together = ("board", "user")
